31451da55584adce840b161e63e79bbd61360704
matiullahmalikzadah <matiullah198611@gmail.com>
2023-11-07 17:53:40 -0800

    basic structure of the car is ready

---
+++ src/test/java/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        CarFeatures audi=new CarFeatures();
+        audi.color="blach";
+        audi.Doors="2";
+        audi.Hp="5000";
+        audi.Model="A90";
+    }
+}
